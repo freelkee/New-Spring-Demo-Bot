33e7dbd94cd0a361e0d9d8647a98cb279024fc2e
freelkee <freelkee@ya.ru>
2023-04-12 01:19:18 +0300

    connect to the database, add keyboard

---
+++ src/main/java/com/example/newspringdemobot/model/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.newspringdemobot.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+}
--- src/main/java/com/example/newspringdemobot/service/TelegramBot.java
+++ src/main/java/com/example/newspringdemobot/service/TelegramBot.java
@@ -1,22 +1,35 @@
 package com.example.newspringdemobot.service;
 
 import com.example.newspringdemobot.config.BotConfig;
+import com.example.newspringdemobot.model.User;
+import com.example.newspringdemobot.model.UserRepository;
+import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+
+    @Autowired
+    private UserRepository userRepository;
 
     final BotConfig config;
 
@@ -34,15 +47,14 @@ public class TelegramBot extends TelegramLongPollingBot {
     public TelegramBot(BotConfig config) {
         this.config = config;
         List<BotCommand> listOfCommands = new ArrayList<>();
-        listOfCommands.add(new BotCommand("/start","get a welcome message"));
-        listOfCommands.add(new BotCommand("/mydata","get your data stored"));
-        listOfCommands.add(new BotCommand("/deletedata","delete your data"));
-        listOfCommands.add(new BotCommand("/help","info how to use this bot"));
-        listOfCommands.add(new BotCommand("/settings","set your preferences"));
-        try{
-            this.execute(new SetMyCommands(listOfCommands,new BotCommandScopeDefault(),null));
-        }
-        catch (TelegramApiException e){
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete your data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
             log.error("Error setting bot's command lis: " + e.getMessage());
         }
     }
@@ -64,18 +76,51 @@ public class TelegramBot extends TelegramLongPollingBot {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
             switch (messageText) {
-                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getUserName());
+                case "/start" -> {
+                    registerUser(update.getMessage());
+                    startCommandReceived(chatId, update.getMessage().getChat().getUserName());
+                }
                 case "/help" -> sendMessage(chatId, HELP_TEXT);
+                case "/mydata" -> myDataCommandReceived(update.getMessage());
                 default -> sendMessage(chatId, "Sorry,command was not recognized");
             }
 
         }
     }
 
+    private void myDataCommandReceived(Message message) {
+        Optional<User> userOptional = userRepository.findById(message.getChatId());
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            sendMessage(message.getChatId(), user.toString());
+            log.info("User get data " + user.getUserName());
+        }
+    }
+
+    private void registerUser(Message message) {
+        if (userRepository.findById(message.getChatId()).isEmpty()) {
+            long chatId = message.getChatId();
+            Chat chat = message.getChat();
+
+            User user = new User();
+
+            user.setChatId(chatId);
+            user.setUserName(chat.getUserName());
+            user.setFirstName(chat.getFirstName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("User saves " + user.getUserName());
+        }
+    }
+
     private void startCommandReceived(long chatId, String name) {
-        String answer = "Hi, " + name + ", nice to meet you!";
+
+        String answer = EmojiParser.parseToUnicode("Hi, " + name + ", nice to meet you!" + ":blush:");
+        //String answer = "Hi, " + name + ", nice to meet you!";
         log.info("Replied to user " + name);
-        sendMessage(chatId,answer);
+        sendMessage(chatId, answer);
+
     }
 
     private void sendMessage(long chatId, String textToSend) {
@@ -83,10 +128,27 @@ public class TelegramBot extends TelegramLongPollingBot {
         message.setChatId(chatId);
         message.setText(textToSend);
 
+        keyboard(message, new String[]{"Test button", "Back"});
+
         try {
             execute(message);
         } catch (TelegramApiException e) {
             log.error("Error occurred: " + e.getMessage());
         }
+    }
+
+    private static void keyboard(SendMessage message, String[] args) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+
+        for (String arg : args) {
+            row.add(arg);
+            keyboardRows.add(row);
+            row = new KeyboardRow();
+        }
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(keyboardMarkup);
     }
 }
